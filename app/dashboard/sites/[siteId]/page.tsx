6f4a881b1d04b120ea656df08bb596fc72268601
Mo.Saif <moh14ab@gmail.com>
2024-12-09 23:58:22 +0100

    add tables to show articles of a site

--- app/dashboard/sites/[siteId]/page.tsx
+++ app/dashboard/sites/[siteId]/page.tsx
@@ -1,9 +1,19 @@
 import prisma from "@/app/utils/db";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { BookOpenText, FileIcon, NotebookPen, Settings } from "lucide-react";
+import { BookOpenText, FileIcon, FilePenLine, FileX, NotebookPen, Settings } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 async function getData(userId: string, siteId: string) {
     const data = await prisma.post.findMany({
@@ -71,8 +81,88 @@ export default async function SiteIdRoute({ params }: { params: Promise<{ siteId
                     </Button>
                 </div>
             ) : (
-                <h1>here is data</h1>
-            )}
+                <div>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>
+                                Articles
+                            </CardTitle>
+                            <CardDescription>
+                                Your place to manage your site articles
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <Table>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead>Image</TableHead>
+                                        <TableHead>Title</TableHead>
+                                        <TableHead>Status</TableHead>
+                                        <TableHead>Created At</TableHead>
+                                        <TableHead className="text-right">Actions</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {data.map((item) => (
+                                        <TableRow key={item.id}>
+                                            <TableCell>
+                                                <Image
+                                                    src={item.image}
+                                                    className="size-16 rounded-md object-cover"
+                                                    width={64}
+                                                    height={64}
+                                                    alt="Article Cover Image"
+                                                />
+                                            </TableCell>
+                                            <TableCell className="font-medium">
+                                                {item.title}
+                                            </TableCell>
+                                            <TableCell >
+                                                <Badge variant={"outline"} className="bg-green-500/10 text-green-500">Published</Badge>
+                                            </TableCell>
+                                            <TableCell>
+                                                {new Intl.DateTimeFormat('locale', {
+                                                    dateStyle: 'medium',
+                                                }).format(item.createdAt)}
+                                            </TableCell>
+
+                                            <TableCell >
+                                                <TooltipProvider>
+                                                    <div className="flex w-full justify-end gap-x-4">
+                                                        <Tooltip>
+                                                            <TooltipTrigger>
+                                                                <Button size="icon" >
+                                                                    <FilePenLine className="size-4" />
+                                                                </Button>
+                                                            </TooltipTrigger>
+                                                            <TooltipContent>
+                                                                <p className="text-primary" >Edit article</p>
+                                                            </TooltipContent>
+
+                                                        </Tooltip>
+                                                        <Tooltip>
+                                                            <TooltipTrigger>
+                                                                <Button size="icon" variant="destructive" aria-label="Delete">
+                                                                    <FileX className="size-4" />
+                                                                </Button>
+                                                            </TooltipTrigger>
+                                                            <TooltipContent>
+                                                                <p className="text-red-500">Delete article</p>
+                                                            </TooltipContent>
+                                                        </Tooltip>
+
+                                                    </div>
+                                                </TooltipProvider>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </CardContent>
+                    </Card>
+                </div >
+            )
+            }
         </>
     );
 }
--- app/dashboard/sites/new/page.tsx
+++ app/dashboard/sites/new/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { CreateSiteAction } from "@/app/actions";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
