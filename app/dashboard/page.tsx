091ee431313a116c1df24b14ff20fe803beaa670
Mo.Saif <moh14ab@gmail.com>
2024-12-09 04:08:40 +0100

    add kinde authintications

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardIndexPage() {
+    return (
+        <div>
+            <h2>Hello Dashboard</h2>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
+import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { Button } from "@/components/ui/button";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { getUser } = getKindeServerSession();
+  const session = await getUser();
   return (
-    <div>
+    <div className="p-10">
       <h1>Hello</h1>
-      <Button>Hello</Button>
+      {session ? (
+        <LogoutLink>
+          <Button>Log out</Button>
+        </LogoutLink>
+      ) : (
+        <div>
+          <RegisterLink>
+            <Button>Sign up</Button>
+          </RegisterLink>
+          <LoginLink>
+            <Button>Sign in</Button>
+          </LoginLink>
+        </div>
+      )}
+
     </div>
   );
 }
