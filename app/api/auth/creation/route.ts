9c5b9a37d9f6a8ea253ce3e4d520bda7a114cb5e
Mo.Saif <moh14ab@gmail.com>
2024-12-11 09:07:27 +0100

    go back

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -31,7 +31,7 @@ export async function GET() {
 
   return NextResponse.redirect(
     process.env.NODE_ENV === "production"
-      ? "https://kitaba-hut.vercel.app/dasboard"
+      ? "https://kitaba-hut.vercel.app/dashboard"
       : "http://localhost:3000/dashboard"
   );
 }
