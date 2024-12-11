6e2f1b0fb5be9ef46e781542d5a6d5e44b2abd2a
Mo.Saif <moh14ab@gmail.com>
2024-12-11 06:56:27 +0100

    testing

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,13 +1,2 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const GET = async (req: { url: any; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { error: any; }): void; new(): any; }; }; }) => {
-    try
-    {
-        console.log("Incoming Request:", req.url);
-        return await handleAuth()(req, res);
-    } catch (error)
-    {
-        console.error("Error in Kinde Auth Callback:", error);
-        res.status(500).json({ error: error.message });
-    }
-};
+export const GET = handleAuth() 
