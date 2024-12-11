eb658526870a4f5053b566d528d2a401b68b7773
Mo.Saif <moh14ab@gmail.com>
2024-12-11 06:22:47 +0100

    trying resolving kinde auth issue in production

--- app/utils/requireUser.ts
+++ app/utils/requireUser.ts
@@ -7,7 +7,7 @@ export const requireUser = async () => {
     const user = await getUser()
 
     if(!user){
-        return redirect('/api/auth/login')
+        return redirect('/api/auth/creation')
     }
 
     return user;
