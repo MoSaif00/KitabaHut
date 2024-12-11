2edc3a5662b85acaf34576db3f91633c5a7c3b3c
Mo.Saif <moh14ab@gmail.com>
2024-12-11 02:29:43 +0100

    try fix build error for props

--- app/dashboard/sites/[siteId]/[articleId]/delete/page.tsx
+++ app/dashboard/sites/[siteId]/[articleId]/delete/page.tsx
@@ -4,7 +4,13 @@ import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 
-export default function DeleteArticleRoute({ params }: { params: { siteId: string, articleId: string; }; }) {
+interface deleteArticleProps {
+    params: {
+        siteId: string;
+        articleId: string;
+    };
+}
+export default function DeleteArticleRoute({ params }: deleteArticleProps) {
     return (
         <div className="flex flex-1 items-center justify-center">
             <Card className="max-w-xl">
