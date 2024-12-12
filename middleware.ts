b7ab7af15859bbc2298bb05e6bfd7d9db4930597
Mo.Saif <moh14ab@gmail.com>
2024-12-12 18:28:31 +0100

    fix strip webhook access from clerk

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/blog(.*)", "/api/webhook", '/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/blog(.*)",
+  "/api/webhook(.*)", // Include all webhook routes
+  "/sign-in(.*)", 
+  "/sign-up(.*)",
+]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    await auth.protect()
+    await auth.protect();
   }
-})
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
